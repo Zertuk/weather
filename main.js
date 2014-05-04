2d6e46de7e81c90482c27673c01db3d88ffb76b2
Justin <zertukis@gmail.com>
2014-05-04 00:04:54 -0500

    sun added when there is no cloud clover, ignored otherwise

--- main.js
+++ main.js
@@ -30,8 +30,23 @@ var getData = function(weather) {
 		var cloud = weather.data.current_condition[0].cloudcover;
 		var time = weather.data.current_condition[0].observation_time;
 		$('#fetch').remove();
-		$('#ground').css("background-color", "#017501");
-		$('body').css("background-color", "#6698FF");
+		var sky = ["#6698FF", "#B6B6B4", "#98AFC7"];
+		var ground = ["#4AA02C", "#387821", "#84a02c"]
+		console.log(cloud);
+		if (cloud > 75) {
+			$('body').css("background-color", sky[1]);
+			$('#ground').css("background-color", ground[2]);
+		}
+		else if (cloud > 40) {
+			$('body').css("background-color", sky[2]);
+			$('#ground').css("background-color", ground[1]);
+		}
+		else {
+			$('body').css("background-color", sky[0]);
+			$('#ground').css("background-color", ground[0]);
+			$('#sun').css("display", "block");
+		}
+		$('#ground').css("background-color", "#4AA02C");		
 		console.log(time);
 		$('#current_temp').text(currentTemp + '\u00B0');
 	}
