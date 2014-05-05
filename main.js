d2af200d8f90e68eb24f36edef0ee5118e8c6ab9
Justin <zertukis@gmail.com>
2014-05-04 23:48:05 -0500

    got rid of some unneccessary code, search animates up

--- main.js
+++ main.js
@@ -13,12 +13,11 @@ $(document).ready(function() {
 			$('#error').text('PLEASE ENTER A LOCATION OR A ZIPCODE');
 		}	
 		else {
-		script.src = 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=' + zip + '&format=json&callback=getData&key=j8xvysb7t9jp2dvw7pwcbgs3'
+		script.src = 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=' + zip + '&num_of_days=5&format=json&callback=getData&key=j8xvysb7t9jp2dvw7pwcbgs3'
 		document.body.appendChild(script);
-
 		}
 		script = null;
-		$(script).remove();;
+		$(script).remove();
 		script = document.createElement('script');
 	})
 });
@@ -29,14 +28,14 @@ var getData = function(weather) {
 		return;
 	}
 	else {
+		$('#error').text('');
 		var currentTemp = weather.data.current_condition[0].temp_F;
 		var cloud = weather.data.current_condition[0].cloudcover;
-		var time = weather.data.current_condition[0].observation_time;
-		var areaName = weather.data.areaName;
-		console.log(areaName);
 		var sky = ["#6698FF", "#B6B6B4", "#98AFC7"];
-		console.log(cloud);
-		$("#fetch").css("margin-top", "0px");
+		$("#fetch").animate({			
+			"margin-top": "0px"		
+		})
+
 		if (cloud > 75) {
 			$('body').css("background-color", sky[1]);
 			$('#other').text("HOW BORING");
@@ -49,9 +48,6 @@ var getData = function(weather) {
 			$('body').css("background-color", sky[0]);
 			$('#other').text("PRETTY BRIGHT");
 		}
-		$('#ground').css("background-color", "#4AA02C");		
-		console.log(time);
-		$('#location').text(areaName);
 		$('#current_temp').text(currentTemp + '\u00B0');
 	}
 }
