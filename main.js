9342102c0bf45d880d271ea397c5e69902395680
Justin <zertukis@gmail.com>
2014-05-04 19:44:52 -0500

    just changing sky color now, temp text added depending on sky, style changes

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 
 	$('form').on('submit', function (inp) {
 		var locInput = $.trim($('#location_input').val().toString());
-		console.log(locInput);
 		inp.preventDefault();
 		var zip = locInput;
 
@@ -16,7 +15,6 @@ $(document).ready(function() {
 		else {
 		script.src = 'http://api.worldweatheronline.com/free/v1/weather.ashx?q=' + zip + '&format=json&callback=getData&key=j8xvysb7t9jp2dvw7pwcbgs3'
 		document.body.appendChild(script);
-		console.log(script);
 		}
 	})
 });
@@ -29,25 +27,26 @@ var getData = function(weather) {
 		var currentTemp = weather.data.current_condition[0].temp_F;
 		var cloud = weather.data.current_condition[0].cloudcover;
 		var time = weather.data.current_condition[0].observation_time;
+		var areaName = weather.data.areaName;
+		console.log(areaName);
 		$('#fetch').remove();
 		var sky = ["#6698FF", "#B6B6B4", "#98AFC7"];
-		var ground = ["#4AA02C", "#387821", "#84a02c"]
 		console.log(cloud);
 		if (cloud > 75) {
 			$('body').css("background-color", sky[1]);
-			$('#ground').css("background-color", ground[2]);
+			$('#other').text("HOW BORING");
 		}
 		else if (cloud > 40) {
 			$('body').css("background-color", sky[2]);
-			$('#ground').css("background-color", ground[1]);
+			$('#other').text("PRETTY GOOD");
 		}
 		else {
 			$('body').css("background-color", sky[0]);
-			$('#ground').css("background-color", ground[0]);
-			$('#sun').css("display", "block");
+			$('#other').text("PRETTY BRIGHT");
 		}
 		$('#ground').css("background-color", "#4AA02C");		
 		console.log(time);
+		$('#location').text(areaName);
 		$('#current_temp').text(currentTemp + '\u00B0');
 	}
 }
